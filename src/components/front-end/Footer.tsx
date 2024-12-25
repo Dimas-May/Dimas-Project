4f07497aceb9251feee4d474eaef12858515101a
Dimas-May <cardo24bekasi@gmail.com>
2024-12-26 00:15:38 +0700

    commit10

---
+++ src/components/front-end/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-accent text-white p-4 text-center mt-16">
+      &copy; Copyright | All Rights Reserved <br /> Unity Flow Shop
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/front-end/TrendingProducts.tsx
+++ src/components/front-end/TrendingProducts.tsx
@@ -2,6 +2,7 @@
 
 import axios from "axios";
 import { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
 
 interface IProduct {
   _id: string;
@@ -24,25 +25,32 @@ const TrendingProducts = () => {
       .catch((err) => console.log(err));
   }, []);
 
-  return <div className="container mt-32">
-    <div className="sm:flex justify-between items-center">
+  return (
+    <div className="container mt-32">
+      <div className="sm:flex justify-between items-center">
         <h2 className="text-4xl font-medium">Trending Products</h2>
 
-
         <div className="text-gray-500 flex gap-4 text-xl mt-4 sm:mt-0">
-            <div className="text-black">New</div>
-            <div>Featured</div>
-            <div>Top Seller</div>
+          <div className="text-black">New</div>
+          <div>Featured</div>
+          <div>Top Seller</div>
         </div>
-    </div>
+      </div>
 
-
-    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-8">
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-8">
         {products.map((item: IProduct) => (
-            <ProductCard key:{item._id} id={item.id} img={item.imgSrc} category:{item.category} title:{item.name} price:{item.price} />
+          <ProductCard
+            key={item._id}
+            id={item._id}
+            img={item.imgSrc}
+            category={item.category}
+            title={item.name}
+            price={item.price}
+          />
         ))}
+      </div>
     </div>
-  </div>
+  );
 };
 
 export default TrendingProducts;
